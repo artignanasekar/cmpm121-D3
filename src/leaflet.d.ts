47a4b4b092664b80afff8a5c2fe74eb0e1cb49e7
arti gnanasekar <arti.gnanam@gmail.com>
2025-11-14 11:50:31 -0800

    made changes to my previous assignment repo, but it was giving me a lot of trouble so copied all my final changes into this one. please look at my D3.a and D3.b assignment to see all my commits of my progress'

--- src/leaflet.d.ts
+++
@@ -1,9 +0,0 @@
-// Use the npm @types/leaflet package for Leaflet global types in this project.
-// Deno will resolve this via the import map in `deno.json` (see "imports").
-/// <reference types="npm:@types/leaflet" />
-
-// Use the npm @types/leaflet package for Leaflet global types in this project.
-// Deno will resolve this via the import map in `deno.json` (see "imports").
-/// <reference types="npm:@types/leaflet" />
-
-export {};
